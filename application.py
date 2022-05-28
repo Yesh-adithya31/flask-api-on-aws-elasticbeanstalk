3c62e1554c604c6667fc04fdf04efb406cbb22aa
Yesh Adithya <57474408+Yesh-adithya31@users.noreply.github.com>
2022-05-28 15:50:37 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,34 @@
 from flask import Flask
+from flask import jsonify,make_response
+from flask import Blueprint, request
+import json
+import pyodbc
+
+
+conn = pyodbc.connect('Driver={SQL Server};Server=tcp:finalresearchdbserver.database.windows.net,1433;Database=SuperMarket;Uid=imesha;Pwd=Mitb@1018;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+ 
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello Test"
+
+@app.route('/viewExample')
+def createExample():
+    try:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM product')
+        rows = [x for x in cursor]
+        cols = [x[0] for x in cursor.description]
+        songs = []
+        for row in rows:
+            song = {}
+            for prop, val in zip(cols, row):
+                song[prop] = val
+            songs.append(song)
+        # Create a string representation of your array of songs.
+        songsJSON = json.dumps(songs)
+        return songsJSON
+    except Exception as e:
+        return f"An Error Occured: {e}"
